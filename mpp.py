006c10a7a51f1276fe092b87832e2d678d63f800
proprefenetre <proprefenetre@gmail.com>
2018-01-28 21:04:44 +0100

    macro expansion :O

--- mpp.py
+++ mpp.py
@@ -9,12 +9,16 @@ import sys
 
 
 teststr = """
+<!--settings 
+    delimiters: { }, | |, % %, (! !)
+    macro: Lorem Morel
+-->
 (include ./LICENSE)
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
 nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
 voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
 kasd gubergren, no sea (color red takimata sanctus) est Lorem ipsum dolor sit
-amet. (it because better are your breasts than wine) Lorem ipsum dolor sit amet,
+amet. (it because better) are (!b your breasts than wine!) Lorem ipsum dolor sit amet,
 consetetur |it sadipscing elitr|, |b sed diam nonumy eirmod tempor| invidunt ut labore et
 dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
 duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
@@ -25,8 +29,8 @@ class Expression:
 
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, delim='( )'):
-        self.delim = delim
+    def __init__(self):
+        self.delim = ['( )']
 
     @property
     @abc.abstractmethod
@@ -40,27 +44,26 @@ class Expression:
         """ key: mandatory attribute """
         raise NotImplementedError
 
-    @property
-    def delim(self):
-        """ delim: mandatory attribute """
-        return self._delim
-
-    @delim.setter
-    def delim(self, val):
-        self._delim = val.split()
-
     def expr(self):
         """ return a compiled regex object that contains the groups 'head' and
             'body'
         """
 
-        exprs = [re.escape(self.delim[0]),
-                 rf"(?P<head>{self.key})",
-                 r"\s",
-                 rf"(?P<body>.*?)",
-                 re.escape(self.delim[1])]
+        starts = []
+        ends = []
+        for p in self.delim:
+            start, end = p.split()
+            starts.append(f"{re.escape(start)}")
+            ends.append(f"{re.escape(end)}")
         
-        return re.compile(''.join(exprs), re.M | re.S)
+        expr = [rf"({'|'.join(starts)})",
+                rf"(?P<head>{self.key})",
+                r"\s*",
+                rf"(?P<body>.*?)",
+                r"\s*?",
+                rf"({'|'.join(ends)})"]
+
+        return re.compile(''.join(expr), re.M | re.S)
 
     @abc.abstractmethod
     def repl(self):
@@ -68,7 +71,7 @@ class Expression:
         raise NotImplementedError
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.key}, delimiters='{self.delim}')"
+        return f"{self.__class__.__name__}({self.__dict__})"
 
 
 class Bold(Expression):
@@ -102,10 +105,61 @@ class Color(Expression):
 class Include(Expression):
 
     key = 'include'
-    prio = 9
+    prio = 8
 
     def repl(self, m):
         return Path(m.group("body")).open().read()
+
+
+class Settings(Expression):
+
+    key = 'settings'
+    prio = 9
+
+    def __init__(self):
+        self.delim = ['<!-- -->']
+ 
+    def repl(self):
+        pass
+    
+    def extract(self, text):
+        
+        settings = {}
+        data = self.expr().search(text).group("body")
+        if data.count('\n') > 0:
+            for line in data.splitlines():
+                key, _, body = line.partition(':')
+                settings[key.strip()] = body.strip()
+        else:
+            key, _, body = data.partition(':')
+            settings[key.strip()] = body.strip()
+        return settings
+        
+
+class Macro(Expression):
+
+    prio = 0
+
+    def __init__(self, key, body):
+        self.key = key
+        self.body = body
+    
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, v):
+        self._key = v
+
+    def expr(self):
+        """ return a compiled regex object that contains the groups 'head' and
+            'body'
+        """
+        return re.compile(rf"(?P<head>{self.key})", re.M | re.S)
+    
+    def repl(self, m):
+        return self.body
 
 
 class Processor:
@@ -117,23 +171,42 @@ class Processor:
     
     def __init__(self):
         self._expressions = []
-        self._extra_delim = ["{ }", "| |"]
+        self._macros = []
+        self._delim = []
 
-    def register(self, expr): 
-        self._expressions.append(expr)
-        for d in self._extra_delim:
-            expr.delim = d
-            self._expressions.append(expr)
+    def settings(self, text):
+        """ extracts a block of settings from the text. """
+        s = Settings()
+        for k, v in s.extract(text).items():
+            print(k, v)
+            if k == "delimiters":
+                self.register_delimiters(*v.split(','))
+            if k == "macro":
+                self.register(Macro(*v.split()))
+
+    def register(self, *expr): 
+        for e in expr:
+            self._expressions.append(e)
+
+    def register_macros(self, *macros): 
+        for m in macros:
+            self._macros.append(m)
+
+    def register_delimiters(self, *delimiters):
+        for m in delimiters:
+            self._delim.append(m)
         
     def _substitute(self, expr, text):
         return expr.expr().sub(expr.repl, text)
 
     def process(self, text):
+        self.settings(text)
         for e in sorted(self._expressions, key=lambda x: x.prio, reverse=True):
+            try:
+                e.delim.extend(self._delim)
+            except:
+                pass
             text = self._substitute(e, text)
-            # for i in self._extra_delim:
-            #     e.delim = i
-            #     text = self._substitute(e, text)
 
         return text
 
@@ -158,30 +231,17 @@ def get_files():
 if __name__ == "__main__":
 
     # inf, outf = get_files()
-    # p = Processor()
-    # p.register(Bold())
-    # p.register(Include())
-    # p.register(Italics())
-    # p.register(Color())
+    p = Processor()
+    p.register(Bold(), Include(), Italics(), Color())
 
-    # print(p._expressions)
+    print(p._expressions)
+    # print(p._macros)
+    print(p.process(teststr))
+    # s = Settings()
+    # x = s.expr().search(teststr).group("body")
+    # print(x.count('\n'))
 
-    delim = ['( )', '{ }']
-    delim.append('| |')
-
-    starts = []
-    ends = []
-    for p in delim:
-        start, end = p.split()
-        starts.append(f"({re.escape(start)})")
-        ends.append(f"({re.escape(end)})")
     
-    expr = ['(' + '|'.join(starts) + ')',
-            rf"(?P<head>it)",
-            r"\s",
-            rf"(?P<body>.*?)",
-            '(' + '|'.join(ends) + ')']
+    
 
-    x = re.compile(''.join(expr), re.M | re.S)
-    print(re.sub(x, lambda m: f"\\textit{{{m.group('body')}}}", teststr))
 
